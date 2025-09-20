98ee1780c3245df626e891e5fcb1d963e775728b
Gokul Krishnan V <gokulkrishnan.v@atts.in>
2025-09-20 09:02:00 +0530

    footer

--- src/pages/Termsandconditions/Termsandconditions.jsx
+++ src/pages/Termsandconditions/Termsandconditions.jsx
@@ -60,29 +60,52 @@ const Termsandconditions = () => {
 
 
             <div className="max-w-7xl mx-auto px-4 py-10 " >
-                <p className='pt-10'>
-                    Terms By accessing this website, you agree to be bound by the following Terms and Conditions of Use, all applicable laws and regulations. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark laws.
+
+                  <h3 className='text-[22px]  pt-2 pb-1'>Terms & Conditions</h3>
+                <p className='pt-2 pb-6'>
+                    Welcome to the official website of Sudha Hospitals (https://sudhahospitals.com/). By accessing and using this website, you agree to comply with and be bound by the following terms and conditions. Please read them carefully. If you do not agree with any part of these terms, please do not use this website. 
                 </p>
-                <h3 className='text-[24px] pb-2 pt-6'>Disclaimer</h3>
-                <p className='pt-2'>
-                    The materials on Sudha Hospital website are provided on an ‘as is’ basis. Sudha Hospital makes no warranties, expressed or implied and hereby disclaims and negates all other warranties including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose or non-infringement of intellectual property or other violation of rights. Further, Sri Ramakrishna Hospital does not warrant or make any representations concerning the accuracy, likely results or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to its website.
+                <h3 className='text-[22px] pt-2 pb-1'>1.Use of Website </h3>
+                <p className='pt-2 pb-6'>
+                    The information provided on this website is intended for general informational purposes only. It should not be considered a substitute for professional medical advice, diagnosis, or treatment. Always consult qualified healthcare professionals for medical concerns. 
                 </p>
-                <h2 className='text-[24px] pb-2 pt-6'>Limitations</h2>
-                <p className='pt-2'>
-                    In no event shall Sudha Hospitals or its suppliers be liable for any damages (including without limitation, damages for loss of data or profit or due to business interruption) arising out of the use or inability to use the materials on the Sudha Hospitals website, even if Sudha Hospital or a Sri Ramakrishna Hospital-authorized representative has been notified orally or in writing of the possibility of such damage. As some jurisdictions do not allow limitations on implied warranties or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+                <h3 className='text-[22px] pt-2 pb-1'>2.Medical Disclaimer </h3>
+                <p className='pt-2 pb-6'>
+                    Sudha Hospitals does not guarantee the accuracy, completeness, or relevance of any medical content provided on this site. The website is not intended to offer specific medical advice, and any reliance on the information is solely at your own risk. 
                 </p>
-                <h2 className='text-[24px] pb-2 pt-6'>Revisions and Errata</h2>
-                <p className='pt-2'>
-                    The materials appearing on Sudha Hospital website could include technical, typographical or photographic errors. Sudha Hospital does not warrant that any of the materials on its website are accurate, complete or current. Sri Ramakrishna Hospital may make changes to the materials contained on its website at any time without notice. Sudha Hospital does not however, make any commitment to update the materials.
+                <h3 className='text-[22px] pt-2 pb-1'>3.Appointment & Services </h3>
+                <p className='pt-2 pb-6'>
+                    Online appointment booking, inquiries, or service-related forms do not guarantee confirmation of services. Our staff will follow up for confirmation, and final decisions will be made based on availability and medical requirements. 
                 </p>
-                <h2 className='text-[24px] pb-2 pt-6'>Site Terms of Use Modifications</h2>
-                <p className='pt-2'>
-                    Sudha Hospital may revise these terms of use for its website at any time without notice. By using this website, you are agreeing to be bound by the then current version of these Terms and Conditions of Use.
+                <h3 className='text-[22px] pt-2 pb-1'>4.Intellectual Property </h3>
+                <p className='pt-2 pb-6'>
+                    All content on this website—including text, images, graphics, logos, videos, and downloadable materials—is the property of Sudha Hospitals unless otherwise stated and is protected by applicable copyright and trademark laws. Unauthorized use, reproduction, or distribution is strictly prohibited. 
                 </p>
-                <h2 className='text-[24px] pb-2 pt-6'>Governing Law</h2>
-                <p className='pt-2 mb-10'>
-                    Any claim relating to Sudha Hospital website shall be governed by the laws of the State of Tamil Nadu without regard to its conflict of law provisions.
+                <h3 className='text-[22px] pt-2 pb-1'>5.Third-party Links </h3>
+                <p className='pt-2  pb-6'>
+                    This website may contain links to external websites that are not operated or controlled by Sudha Hospitals. We are not responsible for the content, accuracy, or privacy practices of such third-party websites. 
                 </p>
+                 <h3 className='text-[22px] pt-2 pb-1'>6.Limitation of Liability </h3>
+                <p className='pt-2 pb-6'>
+                   Sudha Hospitals and its affiliates shall not be held liable for any direct, indirect, incidental, or consequential damages arising out of or related to the use or inability to use this website, or any errors or omissions in the content. 
+                </p>
+                <h3 className='text-[22px] pt-2 pb-1'>7.Privacy Policy</h3>
+                <p className='pt-2 pb-6'>
+                   Use of this website is also governed by our Privacy Policy. By accessing this site, you agree to the practices described therein. 
+                </p>
+
+                 <h3 className='text-[22px] pt-2 pb-2'>7.Modifications to Terms </h3>
+                <p className='pt-2 pb-6'>
+                  Sudha Hospitals reserves the right to modify these terms at any time without prior notice. Continued use of the website after changes are posted will constitute your acceptance of the revised terms. 
+                </p>
+
+                 <h3 className='text-[22px] pt-2 pb-1'>8.Governing Law </h3>
+                <p className='pt-2 pb-6'>
+                 These Terms & Conditions are governed by the laws of the Republic of India. Any disputes arising from the use of this site will fall under the jurisdiction of courts located in Tamil Nadu.  
+                </p>
+
+
+                <p className='pt-10'>For any questions regarding these Terms & Conditions, please contact us through email or phone.  </p>
             </div>
         </div>
     );
