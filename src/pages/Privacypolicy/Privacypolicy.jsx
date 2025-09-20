f6eb917210205c392df0b82ffc63a93ba59b2da8
Gokul Krishnan V <gokulkrishnan.v@atts.in>
2025-09-20 10:11:43 +0530

    img update

--- src/pages/Privacypolicy/Privacypolicy.jsx
+++ src/pages/Privacypolicy/Privacypolicy.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { motion } from 'framer-motion';
-import Banner from "@/assets/privacypolicy/banner.png";
+import Banner from "@/assets/privacypolicy/Banner.png";
 import Breadcrumb from "@/components/Breadcrumb";
 
 
